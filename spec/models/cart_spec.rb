7836073ad557376697304d45fc27ba1121a68a49
bruce919 <bruce919@gmail.com>
2017-01-18 19:57:16 +0800

    初始化 @items

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 class Cart
+	def initialize #任何要初始化的都放這裡
+		@items = []
+	end
+
 	def empty?
-		true
+		@items.empty?
+	end
+
+	def add_item(product_id)
+		@items << product_id #把產品丟進去
 	end
 
 end
